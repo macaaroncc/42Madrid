44fca2d65bb773d12fe7b1d6a8451f2978b72503
Aaron Casado Cabezas <156083611+macaaroncc@users.noreply.github.com>
2025-07-24 11:49:43 +0200

    c01

---
+++ c01/ex07/src/ex07/ft_putnbr.java
@@ -0,0 +1,24 @@
+package ex07;
+
+public class ft_putnbr {
+	
+	
+	public static void main(String[] args) {
+		
+		int entero = 5;
+		
+		int arraymalva = {5};
+		
+		char caracter = '2';
+		
+		String palabra = "hola mundo";
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
